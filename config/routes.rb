248d011f12bf41e9898b21414c90d32c13ca223e
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-27 21:37:50 +0000

    follow devise conventions to set root route to render boards.index

--- config/routes.rb
+++ config/routes.rb
@@ -42,5 +42,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "boards#index"
 end
